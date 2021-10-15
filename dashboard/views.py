0aa6f9872007d469647453341f223e942a9c4642
Peter153 <peterthindi33@gmail.com>
2021-10-15 09:07:43 +0300

    multiple updates

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,17 @@
+from django.core.checks import messages
 from django.shortcuts import render
+from . forms import *
+
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
-    
+
+def notes(request):
+    form = NotesForm()
+    context = {'form':form}
+    return render(request,'dashboard/notes.html',context)
+   
+
+
+      
--- studentstudyportal/settings.py
+++ studentstudyportal/settings.py
@@ -128,3 +128,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
